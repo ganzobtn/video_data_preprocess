aed2e8fc70b76002034f71ef11d6b528ba1821f1
ganzob <gbatnasan5@gmail.com>
2023-09-10 23:20:53 +0400

    added multiprocessing

--- multiprocessing_create_head_hands_stack.py
+++ multiprocessing_create_head_hands_stack.py
@@ -1,3 +1,4 @@
+
 import mediapipe as mp
 import os
 import cv2
@@ -5,6 +6,9 @@ import numpy as np
 import argparse
 
 import time
+import os
+from concurrent.futures import ThreadPoolExecutor
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--data_path', type=str,
@@ -16,21 +20,19 @@ parser.add_argument('--dest_path', type=str,
 opt = parser.parse_args()
 
 
-data_path = '/home/ganzorig/Datas/chalearn_processed_full/color/'
-dest_path = '/home/ganzorig/Datas/chalearn_processed_full_skeleton/color/'
-
-MARGIN =10
+data_path = opt.data_path#'/home/ganzorig/Datas/chalearn_processed_full/color/'
+dest_path = opt.dest_path#'/home/ganzorig/Datas/chalearn_processed_full_skeleton/color/'
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_holistic = mp.solutions.holistic
 
 
-
 MARGIN = 10  # pixels
 FONT_SIZE = 1
 FONT_THICKNESS = 1
 HANDEDNESS_TEXT_COLOR = (88, 205, 54) # vibrant green
+
 
 
 def get_features(data_path, dest_path):
@@ -122,6 +124,9 @@ def get_crop(image, landmarks):
     return min_x,min_y,max_x,max_y, text_x,text_y
 
 def get_head_hands(data_path, dest_path):
+    if not os.path.exists(dest_path_new):      
+        os.makedirs(dest_path_new)
+    
     cap = cv2.VideoCapture(data_path)
 
     frame_width, frame_height = int(cap.get(3)), int(cap.get(4))
@@ -251,32 +256,58 @@ def get_head_hands(data_path, dest_path):
     cap.release()
     out.release()
 
+# Define your video processing function
+def process_video(file_path):
+    root =file_path[0],file= file_path[1],dest_path_new = file_path[2] 
+    if not os.path.exists(dest_path_new):      
+        os.makedirs(dest_path_new)
+    # You can return some result if needed
+    video_path = os.path.join(root,file)
+
+    get_head_hands(data_path= video_path, dest_path = os.path.join(dest_path_new,file))
+
+    return f"Processed {video_path}"
+
+# def main():
+#     video_dir = "path_to_video_directory"  # Replace with the path to your video directory
+#     video_files = [os.path.join(video_dir, file) for file in os.listdir(video_dir) if file.endswith(".mp4")]
+
+#     # Create a ThreadPoolExecutor with the number of CPU cores you want to use
+#     num_cpus = os.cpu_count()  # This gets the number of available CPU cores
+#     with ThreadPoolExecutor(max_workers=num_cpus) as executor:
+#         # Process videos in parallel
+#         results = list(executor.map(process_video, video_files))
+
+#     # Print the results
+#     for result in results:
+#         print(result)
+
+
 def main():
+    file_list = []
     for (root,dirs,files) in os.walk(data_path, topdown=True):
-    #print (root)
-    #print (dirs)
-    #print (files)
         for file in files:
             if file.endswith('.mp4'):
                 #print(os.path.join(root,file))
                 dest_path_new = root.replace(data_path,dest_path)
-                if not os.path.exists(dest_path_new):      
-                    os.makedirs(dest_path_new)
+                #if not os.path.exists(dest_path_new):      
+                #    os.makedirs(dest_path_new)
                 #createFolder()
-
+                file_list.append([root,file,dest_path_new])
                 #get_features(data_path=os.path.join(root,file), dest_path = os.path.join(dest_path_new,file))
-                get_head_hands(data_path=os.path.join(root,file), dest_path = os.path.join(dest_path_new,file))
+
+    num_cpus = os.cpu_count()  # This gets the number of available CPU cores
+    with ThreadPoolExecutor(max_workers=num_cpus) as executor:
+        # Process videos in parallel
+        results = list(executor.map(process_video, file_list))
+
+    # Print the results
+    for result in results:
+        print(result)
+
+
         print ('--------------------------------')
 
 
-
-if __name__=='__main__':
-
-    #data_path = '/tmp/data/wlasl_2000/WLASL2000'
-    #dest_path = '/tmp/data/wlasl_2000/wlasl_2000_head_hands_stack/WLASL2000'
-    print("Hi")
+if __name__ == "__main__":
     main()
-    #pass
-
-
-    #get_head_hands(data_path=data_path,dest_path= dest_path)
