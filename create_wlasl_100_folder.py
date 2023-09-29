ec0b77232d9b44a05f4586e07277d5ba41eb77c2
ganzob <gbatnasan5@gmail.com>
2023-09-29 17:20:42 +0400

    create wlasl100 and wlasl2000 sorted folder

---
+++ create_wlasl_100_folder.py
@@ -0,0 +1,12 @@
+
+import os
+import json
+
+import pickle
+
+
+wlasl_videos_folder = '/projects/data/wlasl_2000/WLASL2000'
+
+json_file = json.load('projects/data/wlasl_2000/100.json')
+
+print(json_file )
